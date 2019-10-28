6fb3fea2667dbf0fddc26e28682cb38f15186dad
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2019-10-28 12:30:46 +0000

    Started adding code for dealing with build variations.
    Currently setup for just gui/cli interface variants.

--- recipes/prismatic/build.sh
+++ recipes/prismatic/build.sh
@@ -2,7 +2,19 @@
 
 mkdir build && cd build 
 
-cmake -D PRISMATIC_ENABLE_GUI=1 \
+enable_cli=0
+enable_gui=0
+enable_py=0
+
+
+if [[ $interface == "cli" ]]; then
+	enable_cli=1
+elif [[ $interface == "gui" ]]; then
+	enable_gui=1
+fi
+
+cmake -D PRISMATIC_ENABLE_GUI=$enable_gui \
+	-D PRISMATIC_ENABLE_CLI=$enable_cli \
 	-D CMAKE_INSTALL_PREFIX=$PREFIX \
 	-D CMAKE_PREFIX_PATH=${PREFIX} \
 	../ 
