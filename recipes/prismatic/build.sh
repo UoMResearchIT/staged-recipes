70919d3b11ebd5276b072810a201be63dea5bb52
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2019-10-31 15:12:01 +0000

    Added initial python build commands - the build fails (on OSX) at the stage of loading the core library though.

--- recipes/prismatic/build.sh
+++ recipes/prismatic/build.sh
@@ -1,10 +1,11 @@
 
 
-mkdir build && cd build 
 
 enable_cli=0
 enable_gui=0
 enable_py=0
+enable_double=0
+enable_gpu=0
 
 
 if [[ $interface == "cli" ]]; then
@@ -13,12 +14,22 @@ elif [[ $interface == "gui" ]]; then
 	enable_gui=1
 fi
 
-cmake -D PRISMATIC_ENABLE_GUI=$enable_gui \
-	-D PRISMATIC_ENABLE_CLI=$enable_cli \
-	-D CMAKE_INSTALL_PREFIX=$PREFIX \
-	-D CMAKE_PREFIX_PATH=${PREFIX} \
-	../ 
+if [[ $interface != "py" ]]; then
 
-make  -j${CPU_COUNT}
+	mkdir build && cd build 
 
-make install
+	cmake -D PRISMATIC_ENABLE_GUI=$enable_gui \
+		-D PRISMATIC_ENABLE_CLI=$enable_cli \
+		-D PRISMATIC_ENABLE_DOUBLE_PRECISION=$enable_double \
+		-D PRISMATIC_ENABLE_GPU=$enable_gpu \
+		-D CMAKE_INSTALL_PREFIX=$PREFIX \
+		-D CMAKE_PREFIX_PATH=${PREFIX} \
+		../ 
+
+	make  -j${CPU_COUNT}
+
+	make install
+
+else
+	python3 setup.py install --debug
+fi
