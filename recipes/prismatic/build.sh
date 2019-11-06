0d05011175bb2a0d352482f891dec314e4ebae01
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2019-11-06 13:18:54 +0000

    Got pyprismatic build working on linux!
    Needs much tidying up still.

--- recipes/prismatic/build.sh
+++ recipes/prismatic/build.sh
@@ -7,29 +7,40 @@ enable_py=0
 enable_double=0
 enable_gpu=0
 
-
 if [[ $interface == "cli" ]]; then
 	enable_cli=1
+	extra_opts=""
 elif [[ $interface == "gui" ]]; then
 	enable_gui=1
+	extra_opts=""
+elif [[ $interface == "py" ]]; then
+	enable_py=1
+	extra_opts="-D "
 fi
 
-if [[ $interface != "py" ]]; then
 
-	mkdir build && cd build 
 
-	cmake -D PRISMATIC_ENABLE_GUI=$enable_gui \
-		-D PRISMATIC_ENABLE_CLI=$enable_cli \
-		-D PRISMATIC_ENABLE_DOUBLE_PRECISION=$enable_double \
-		-D PRISMATIC_ENABLE_GPU=$enable_gpu \
-		-D CMAKE_INSTALL_PREFIX=$PREFIX \
-		-D CMAKE_PREFIX_PATH=${PREFIX} \
-		../ 
+mkdir build && cd build 
 
-	make  -j${CPU_COUNT}
+cmake -D PRISMATIC_ENABLE_GUI=$enable_gui \
+	-D PRISMATIC_ENABLE_CLI=$enable_cli \
+	-D PRISMATIC_ENABLE_DOUBLE_PRECISION=$enable_double \
+	-D PRISMATIC_ENABLE_GPU=$enable_gpu \
+	-D PRISMATIC_ENABLE_PYPRISMATIC=$enable_py \
+	-D CMAKE_INSTALL_PREFIX=$PREFIX \
+	-D CMAKE_PREFIX_PATH=${PREFIX} \
+	../ 
 
-	make install
+make  -j${CPU_COUNT}
 
+
+if [[ $interface == "py" ]]; then
+	install_dir=${PREFIX}/lib/python3.7/site-packages/pyprismatic
+	mkdir $install_dir
+	cp core.* $install_dir
+	cp ../pyprismatic/* $install_dir
 else
-	python3 setup.py install --debug
+	make install
 fi
+
+
